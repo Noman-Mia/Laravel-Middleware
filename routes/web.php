45667d38afa369a70aa963165e8dfc110ce348c3
Noman Mia <nomanislamnishad@gmail.com>
2024-12-23 14:36:05 +0600

    added action1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\DemoController;
+use App\Http\Middleware\DemoMiddaleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello',[DemoController::class,'DemoAction'])->middleware([DemoMiddaleware::class]);
